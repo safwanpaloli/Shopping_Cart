da3dfc01b7e58064d045bf71aec326ae422fa17a
Safwan Paloli <70832122+safwanpaloli@users.noreply.github.com>
2023-02-20 11:25:18 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -5,13 +5,13 @@ const {addtoProduct, deleteProduct,adminLogin} = require('../controllers/adminCo
 const verifyToken = (req, res , next ) => {
     let auth  = req.headers.authorization;
     if(auth===undefined){
-            res.status(401).json({error : 'no token provided'})
+           return res.status(401).json({error : 'no token provided'})
     }
      
     let token = auth.split(" ")[1]
     jwt.verify(token,"secret",(err)=>{
         if(err){
-            res.status(500).json({error:'authentication failed'})
+            return res.status(500).json({error:'authentication failed'})
         }else{
             next()
         }
